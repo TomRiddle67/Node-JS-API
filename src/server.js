93f6530d4778dee34e0f07a6c66a414ed139d566
TomRiddle67 <courage0047@gmail.com>
2026-04-28 12:50:01 +0000

    feat: add prisma and supabase connection

--- src/server.js
+++ src/server.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import {config} from 'dotenv';
+import { connectDB, disconnectDB } from './config/db.js';
+
+// Import Routes
 import movie_routes from './routes/movie_routes.js';
+
+config();
+connectDB();
 
 const app = express();
 
@@ -11,4 +18,29 @@ app.use('/movies', movie_routes);
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`server running on PORT ${PORT}`)
+});
+
+// Handle database connection errors
+process.on('unhandledRejection', (err) => {
+    console.error('Unhandled Rejection:', err);
+    server.close(async () => {
+        await disconnectDB();
+        process.exit(1);
+    });
+});
+
+// Handle uncaught exceptions
+process.on('unhandledException', async (err) => {
+    console.error('Uncaught Exception:', err);
+    await disconnectDB();
+    process.exit(1);
+    });
+
+// Graceful shutdown
+process.on('SIGTERM', async () => {
+    console.error('SIGTERM received, shutting down gracefully');
+    server.close(async () => {
+        await disconnectDB();
+        process.exit(1);
+    });
 });
